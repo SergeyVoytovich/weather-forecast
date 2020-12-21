057d53d16c19c9ed5ea9d2a7b2332d76e47eaf14
SergeyVoytovich <s.voytovich@outlook.com>
2020-12-21 12:12:13 -0800

    Create open weathermap repository

---
+++ src/WeatherForecast/Data/WeatherForecast.Data.OpenWeathermap/IOpenWeathermapConfig.cs
@@ -0,0 +1,13 @@
+﻿namespace WeatherForecast.Data.OpenWeathermap
+{
+    /// <summary>
+    /// Configuration for open wathermap 
+    /// </summary>
+    public interface IOpenWeathermapConfig
+    {
+        string ApiKey { get; set; }
+        string CountryCode { get; set; }
+        Units Units { get; set; }
+        string Language { get; set; }
+    }
+}
---
+++ src/WeatherForecast/Data/WeatherForecast.Data.OpenWeathermap/Units.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherForecast.Data.OpenWeathermap
+{
+    public enum Units
+    {
+        Standard = 0,
+        Metric = 1,
+        Imperial = 2
+    }
+}
