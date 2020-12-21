933d94965b07102c9e55bfec4ccba4c1ee5930c8
SergeyVoytovich <s.voytovich@outlook.com>
2020-12-21 11:31:31 -0800

    Create domain models

---
+++ src/WeatherForecast/Common/Common.Interfaces/Models/ICity.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Common.Interfaces.Models
+{
+    public interface ICity
+    {
+        int Id { get; set; }
+        string Name { get; set; }
+        int ZipCode { get; set; }
+        
+        IList<IWeather> Weather { get; set; }
+    }
+}
---
+++ src/WeatherForecast/Common/Common.Interfaces/Models/Weather.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Common.Interfaces.Models
+{
+    public interface IWeather
+    {
+        DateTime DateTime { get; set; }
+        bool IsCurrent => DateTime.Date == DateTime.Now.Date;
+        decimal Temperature { get; set; }
+        decimal Humidity { get; set; }
+        decimal WindSpeed { get; set; }
+    }
+}
