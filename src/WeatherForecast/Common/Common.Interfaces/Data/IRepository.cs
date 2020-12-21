22c29a6df10159884d2750a3afec602a315002c6
SergeyVoytovich <s.voytovich@outlook.com>
2020-12-21 11:38:17 -0800

    Declare repository interface

---
+++ src/WeatherForecast/Common/Common.Interfaces/Data/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using Common.Interfaces.Models;
+
+namespace Common.Interfaces.Data
+{
+    public interface IRepository
+    {
+        ICity Get(string name);
+        ICity Get(int zipCode);
+    }
+}
